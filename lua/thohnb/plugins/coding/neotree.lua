9d0f97e87eba701ae8626ee1cac6ac01b1f80d1f
Bùi Nguyễn Hoàng Thọ <thohnb@gmail.com>
2024-03-27 09:19:31 +0700

    Cnage config neotree a little

--- lua/thohnb/plugins/coding/neotree.lua
+++ lua/thohnb/plugins/coding/neotree.lua
@@ -201,9 +201,9 @@ return {
               },
             },
             follow_current_file = {
-              enabled = false, -- This will find and focus the file in the active buffer every time
+              enabled = true, -- This will find and focus the file in the active buffer every time
               --               -- the current file is changed while the tree is open.
-              leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+              leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
             },
             group_empty_dirs = false, -- when true, empty folders will be grouped together
             hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -291,6 +291,6 @@ return {
           }
         })
   
-        vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+        -- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
       end
   }
